c6559ab593c795d7c43999295cd7aac74c6e2de0
ameliaritger <47021807+ameliaritger@users.noreply.github.com>
2023-04-02 17:14:50 -0700

    set gain values appropriately.

--- c_code/test-alarm-oversample/CONFIG.h
+++ c_code/test-alarm-oversample/CONFIG.h
@@ -1,20 +1,20 @@
 #ifndef config_h
 #define config_h
 
-#define FILE_NAME "test-12.csv" //KEEP THIS AT OR UNDER 8 CHARACTERS, not including '.csv' extension
+#define FILE_NAME "0402sd.csv" //KEEP THIS AT OR UNDER 8 CHARACTERS, not including '.csv' extension
 
 constexpr time_t ALARM_INTERVAL {10};   // alarm interval (in seconds)
-constexpr time_t START_DAY {1};   // set the day of the month start time (1-31)
-constexpr time_t START_HOUR {19};   // set the hours start time (0-23)
-constexpr time_t START_MIN {41};   // set the minutes start time (0-59)
+constexpr time_t START_DAY {2};   // set the day of the month start time (1-31)
+constexpr time_t START_HOUR {16};   // set the hours start time (0-23)
+constexpr time_t START_MIN {17};   // set the minutes start time (0-59)
 
-#define OVERSAMPLE_VALUE 64 // set oversampling value. DEFAULT: 64. options: 16, 64, and 256 //add 1000,2000,7000 (seconds) to delayTime for total sampling rate
+#define OVERSAMPLE_VALUE 16 // set oversampling value. DEFAULT: 64. options: 16, 64, and 256 //add 1000,2000,7000 (seconds) to delayTime for total sampling rate
 
 // Set ADC gain values. DO NOT CHANGE, see table below for reference.
-#define ADS1015_GAIN_VAL GAIN_TWOTHIRDS   // Recommended: GAIN_ONE
-#define ADS1015_GAIN_MULT 3              // Change this number based on gain selected.
-#define ADS1115_GAIN_VAL GAIN_TWOTHIRDS  // Recommended: GAIN_FOUR
-#define ADS1115_GAIN_MULT 0.1875          // Change this number based on gain selected.
+#define ADS1015_GAIN_VAL GAIN_ONE   // Recommended: GAIN_ONE
+#define ADS1015_GAIN_MULT 2             // Change this number based on gain selected.
+#define ADS1115_GAIN_VAL GAIN_FOUR  // Recommended: GAIN_FOUR
+#define ADS1115_GAIN_MULT 0.03125          // Change this number based on gain selected.
 
 //                                                                ADS1015  ADS1115
 //                                                                -------  -------
